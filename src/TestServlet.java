e880092e2fdacb4201b8bf947c753fc05f0256d8
manso156 <manso156@GB150WS39.WIN.ecf.utoronto.ca>
2020-11-21 13:07:56 -0500

    init

---
+++ src/TestServlet.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.DbUtil;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		try{
+			Connection connection = DbUtil.getConnection();
+			connection.createStatement().executeQuery("select * from Model");
+			out.print("connection successful");
+		}catch(Exception e){
+			e.printStackTrace();
+			out.print("connection failed");
+			
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
